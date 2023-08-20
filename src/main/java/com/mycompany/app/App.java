c745b9087a523f835764c00fb674ff2497c26ba0
Vikas G N <88074382+vikkkz@users.noreply.github.com>
2023-08-20 21:30:31 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,34 +3,19 @@ package com.mycompany.app;
 /**
  * Hello world!
  */
-public class FactorialCalculator {
+public class App {
 
-    public static int calculateFactorial(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
-        } else {
-            return n * calculateFactorial(n - 1);
-        }
-    }
+    private static final String MESSAGE = "Hello World!";
+
+    public App() {}
 
     public static void main(String[] args) {
-        int number = 5;
-        int factorial = calculateFactorial(number);
-        System.out.println("Factorial of " + number + " is: " + factorial);
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
     }
 }
 
-// public class App {
 
-//     private static final String MESSAGE = "Hello World!";
-
-//     public App() {}
-
-//     public static void main(String[] args) {
-//         System.out.println(MESSAGE);
-//     }
-
-//     public String getMessage() {
-//         return MESSAGE;
-//     }
-// }
